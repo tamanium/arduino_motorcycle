20a49ced77f2791120c551796981eacc0c24f1f1
tamanium <111425337+tamanium@users.noreply.github.com>
2024-10-03 21:21:52 +0900

    Update arduinoSource.ino

--- arduinoSource.ino
+++ arduinoSource.ino
@@ -1,59 +1,219 @@
+// --------------------ライブラリ--------------------
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include <SPI.h>
+
+// --------------------定数--------------------
 #define ON true
-#define OFF false;
+#define OFF false
+
+#define posInterval  200
+#define wnkInterval  100
+#define bzzInterbal  50
+
+// --------------------ピン定義--------------------
+#define TFT_MOSI  3
+#define TFT_SCLK  2
+#define TFT_CS    6
+#define TFT_RST   8
+#define TFT_DC    7
+
+// ギアポジション入力ピン
+#define pos1  9
+#define pos2  10
+#define pos3  11
+#define pos4  12
+#define posN  13
+
+// ウインカー対応ピン
+#define wnkRight 15
+#define wnkLeft 14
+
 // ビープ音を発生させるピン
 #define bzzPin 29
 
-// ギアポジション入力ピン
-#define pos1 1;
-#define pos2 2;
-#define pos3 3;
-#define pos4 4;
-#define posN 5;
 
-// ウインカー対応ピン
-#define wnkRight 10
-#define wnkLeft  11
+// --------------------変数--------------------
+char exPos = '-';
 
+unsigned long startTime = 0;
+unsigned long posTime = 0;
+unsigned long wnkTime = 0;
+unsigned long bzzTime = 0;
 
-void setup(){
-  // ギアポジション入力ピン
+bool wnkRightStatus = false;
+bool wnkLeftStatus = false;
+
+// --------------------インスタンス--------------------
+Adafruit_ST7735 tft = Adafruit_ST7735(&SPI, TFT_CS, TFT_DC, TFT_RST);
+
+void setup(void) {
+
   pinMode(pos1, INPUT_PULLUP);
   pinMode(pos2, INPUT_PULLUP);
   pinMode(pos3, INPUT_PULLUP);
   pinMode(pos4, INPUT_PULLUP);
   pinMode(posN, INPUT_PULLUP);
-}
 
-void loop(){
-  // ギアポジションの表示値取得
-  char dispChar = getPosChar();
+  pinMode(wnkRight, INPUT_PULLUP);
+  pinMode(wnkLeft, INPUT_PULLUP);
+
+  SPI.setTX(TFT_MOSI);
+  SPI.setSCK(TFT_SCLK);
+
+  // 初期化
+  tft.initR(INITR_BLACKTAB);
+  // 画面向き：右に90度回転
+  tft.setRotation(1);
+  // 黒背景で表示リセット
+  tft.fillScreen(ST77XX_BLACK);
   
+  tft.setCursor(0, 0);
+  tft.setTextColor(ST77XX_GREEN);
+  tft.setTextSize(3);
+  tft.setTextWrap(true);
+  tft.print("hello");
+
+  delay(2000); // Pause for 2 seconds
+  
+  tft.fillScreen(ST77XX_BLACK);
+
+  tft.setCursor(0, 8*3);
+
+  tft.setTextSize(1);
+  tft.print("gear");
+
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextSize(3);
+  tft.setCursor(0, 0);
+  tft.print(exPos);
+
+/*
+  tone(tonePin, 220);
+  delay(100);
+  noTone(tonePin);
+  delay(150);
+  tone(tonePin, 220);
+  delay(100);
+  noTone(tonePin);
+  delay(150);
+  */
+
+  //起動時の時間を取得
+  startTime = millis();
+  posTime = startTime + posInterval;
+  wnkTime = startTime + wnkInterval;
 }
 
-// ギアポジションの取得
-char getPosChar(){
-  char dispChar = '-';
-  if((digitalRead(pos1) == LOW){
-    dispChar = '1';
+void loop() {
+  unsigned long time = startTime - millis();
+
+  if(digitalRead(wnkLeft) == LOW){
+    if(wnkLeftStatus == OFF){
+      tft.setCursor(0, 50+8*3);
+      bzzTime = time + bzzInterbal;
+      tft.print(bzzTime);
+    }
+    wnkLeftStatus = ON;
   }
-  else if((digitalRead(pos1) == LOW){
-    dispChar = '2;
+  else{
+    if(wnkLeftStatus == ON){
+      tft.setCursor(0, 50+8*3);
+      bzzTime = time + bzzInterbal;
+      tft.print(bzzTime);
+    }
+    wnkLeftStatus = OFF;
   }
-  else if((digitalRead(pos1) == LOW){
-    dispChar = '3';
+
+  if(digitalRead(wnkRight) == LOW){
+    if(wnkRightStatus == OFF){
+      tft.setCursor(0, 50+8*3);
+      bzzTime = time + bzzInterbal;
+      tft.print(bzzTime);
+    }
+    wnkRightStatus = ON;
   }
-  else if((digitalRead(pos1) == LOW){
-    dispChar = '4';
+  else{
+    if(wnkRightStatus == ON){
+      tft.setCursor(0, 50+8*3);
+      bzzTime = time + bzzInterbal;
+      tft.print(bzzTime);
+    }
+    wnkRightStatus = OFF;
   }
-  else if((digitalRead(pos1) == LOW){
-    dispChar = 'N';
+
+  if(bzzTime > 0 && bzzTime <= time ){
+    tft.fillRect(0, 50+8*3, 160, 8*3, ST77XX_GREEN);
+    bzzTime = 0;
   }
-  return dispChar;
+   
+
+  // ギアポジ表示処理
+  //if(posTime <= time ){
+    // ギアポジション取得
+    char pos = getPos();
+
+    if(pos != exPos){
+      tft.fillRect(0,0,6*3,8*3,ST77XX_BLACK);
+      tft.setCursor(0, 0);
+      tft.print(pos);
+      exPos = pos;
+    }
+    posTime += posInterval;  
+  //}
+
+  // ウインカー表示処理
+  //if(wnkTime <= time){
+    
+      if(wnkRightStatus == ON){
+        tft.setCursor(0, 50);
+        tft.print('<');
+      }
+      else{
+        tft.fillRect(0, 50, 6*3, 8*3, ST77XX_BLACK);
+      }
+
+      if(wnkLeftStatus == ON){
+        tft.setCursor(6*3, 50);
+        tft.print('>');
+      }
+      else{
+        tft.fillRect(6*3, 50, 6*3, 8*3, ST77XX_BLACK);
+      }
+
+    wnkTime += wnkInterval;
+  //}
+
+  //delay(200);
 }
 
-bool getWinkerStatus(winkerPin){
-  if(digitalRead(winkerPin) == HIGH){
-    return true;
+//ギアポジション取得
+char getPos(){
+  // ギアポジ表示値
+  char pos = '-';
+  // ギアポジ取得
+  if(digitalRead(pos1) == LOW){
+    pos = '1';
   }
-  return flse;
+  else if(digitalRead(pos2) == LOW){
+    pos = '2';
+  }
+  else if(digitalRead(pos3) == LOW){
+    pos = '3';
+  }
+  else if(digitalRead(pos4) == LOW){
+    pos = '4';
+  }
+  else if(digitalRead(posN) == LOW){
+    pos = 'N';
+  }
+  return pos;
+}
+
+
+void testdrawtext(char *text, uint16_t color) {
+  tft.setCursor(0, 0);
+  tft.setTextColor(color);
+  tft.setTextWrap(true);
+  tft.print(text);
 }
