36905c5f4364c01a30f360f8e3fe9a83be4c310e
tamanium <111425337+tamanium@users.noreply.github.com>
2024-10-04 12:58:18 +0900

    Create PinDefine.h

---
+++ PinDefine.h
@@ -0,0 +1,25 @@
+#ifndef PINDEFINE
+#define PINDEFINE
+
+// --------------------ピン定義--------------------
+#define TFT_MOSI  3
+#define TFT_SCLK  2
+#define TFT_CS    6
+#define TFT_RST   8
+#define TFT_DC    7
+
+// ギアポジション入力ピン
+#define pos1  9
+#define pos2  10
+#define pos3  11
+#define pos4  12
+#define posN  13
+
+// ウインカー対応ピン
+#define wnkRight 15
+#define wnkLeft 14
+
+// ビープ音を発生させるピン
+#define bzzPin 29
+
+#endif
