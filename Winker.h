462435bf9c00b23c6daeadece399a7c42e8889cd
tamanium <111425337+tamanium@users.noreply.github.com>
2024-10-04 12:52:43 +0900

    Create Winker.h

---
+++ Winker.h
@@ -0,0 +1,25 @@
+#ifndef WINKER_H_INCLUDE
+#define WINKER_H_INCLUDE
+
+class Winker{
+
+  private:
+    // ウインカー状態読み取りピン
+    int wnkPin;
+    // ウインカー点灯状態
+    bool exStatus
+
+  public:
+    Winker(int pin);
+}
+/**
+ *コンストラクタ
+ */
+Winker::Winker(int pin){
+  // ウインカー状態読み取りピンを定義
+  wnkpin = pin;
+  // ウインカー点灯状態を定義（デフォルト値：false）
+  exStatus = false;
+}
+
+#endif
