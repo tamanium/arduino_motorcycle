dc98db13621a8939729d53280b829a94fbf36a90
tamama <curetamama@gmail.com>
2024-10-06 23:32:22 +0900

    10/06

--- arduinoSource.ino
+++ arduinoSource.ino
@@ -48,42 +48,50 @@ unsigned long bzzTime = 1;
 bool wnkRightStatus = false;
 bool wnkLeftStatus = false;
 
+bool isWnkRight = false;
+bool isWnkLeft = false;
+
+char  realTime[6] = " 0:00";
+unsigned long realTimeLong = 0;
+
 // --------------------インスタンス--------------------
 Adafruit_ST7735 tft = Adafruit_ST7735(&SPI, TFT_CS, TFT_DC, TFT_RST);
 
 void setup(void) {
+  
+  Serial.begin(9600);
 
+  //ピンモード設定
   pinMode(POS1_PIN, INPUT_PULLUP);
   pinMode(POS2_PIN, INPUT_PULLUP);
   pinMode(POS3_PIN, INPUT_PULLUP);
   pinMode(POS4_PIN, INPUT_PULLUP);
   pinMode(POSN_PIN, INPUT_PULLUP);
-
   pinMode(WNK_RIGHT, INPUT_PULLUP);
   pinMode(WNK_LEFT, INPUT_PULLUP);
 
+  //SPI接続設定
   SPI.setTX(TFT_MOSI);
   SPI.setSCK(TFT_SCLK);
 
-  // 初期化
+  // ディスプレイ初期化
   tft.initR(INITR_BLACKTAB);
-  // 画面向き：右に90度回転
-  tft.setRotation(1);
+  // 画面向き
+  tft.setRotation(3);
   // 黒背景で表示リセット
   tft.fillScreen(ST77XX_BLACK);
   
+  // 初期表示
   tft.setCursor(0, 0);
   tft.setTextColor(ST77XX_GREEN);
   tft.setTextSize(3);
   tft.setTextWrap(true);
   tft.print("hello");
-
-  delay(2000); // Pause for 2 seconds
+  delay(2000);
   
+  // ギアポジション表示開始
   tft.fillScreen(ST77XX_BLACK);
-
   tft.setCursor(8*5+4, 8*8);
-
   tft.setTextSize(3);
   tft.print("gear");
 
@@ -91,6 +99,12 @@ void setup(void) {
   tft.setTextSize(8);
   tft.setCursor(8*7+4, 0);
   tft.print(exPos);
+
+  // 時計表示開始
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextSize(2);
+  tft.setCursor(0,128-8*2);
+  tft.print("00:00");
 
 /*
   tone(tonePin, 220);
@@ -111,10 +125,41 @@ void setup(void) {
 
 void loop() {
   unsigned long time = millis() - startTime;
+  // --------------------時計機能--------------------
+  // システム時間(秒)取得
+  unsigned long realTotalSec = time/1000;
+
+  //int realSec = realTotalSec%60;
+  //int realMins = realTotalSec/60;
+
+  int _realTimeMin1 = (realTotalSec%60)%10;
+  int _realTimeMin2 = (realTotalSec%60)/10;
+  int _realTimeHour1 = (realTotalSec/60)%10;
+  int _realTimeHour2 = (realTotalSec/60)/10;
+  char _realTime[6] = "  :  ";
+  _realTime[4] = '0'+_realTimeMin1;
+  _realTime[3] = '0'+_realTimeMin2;
+  _realTime[1] = '0'+_realTimeHour1;
+  _realTime[0] = '0'+_realTimeHour2;
+  
+
+
+  // フォント設定
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextSize(2);
+  
+  for(int i=4; i>=0; i--){
+    if(realTime[i] != _realTime[i]){
+      tft.fillRect(6*2*i, 128-8*2, 6*2, 8*2, ST7735_BLACK);
+      tft.setCursor(6*2*i, 128-8*2);
+      tft.print(_realTime[i]);
+      realTime[i] = _realTime[i];
+    }
+  }
 
   if(digitalRead(wnkLeft) == LOW){
     if(wnkLeftStatus == OFF){
-      tft.setCursor(0, 120);
+      tft.setCursor(160-6*3, 120);
       if(bzzTime == 0){
         bzzTime = time + bzzInterbal;
         tft.setTextSize(1);
@@ -125,7 +170,7 @@ void loop() {
   }
   else{
     if(wnkLeftStatus == ON){
-      tft.setCursor(0, 120);
+      tft.setCursor(160-6*3, 120);
       if(bzzTime == 0){
         bzzTime = time + bzzInterbal;
         tft.setTextSize(1);
@@ -137,7 +182,7 @@ void loop() {
 
   if(digitalRead(wnkRight) == LOW){
     if(wnkRightStatus == OFF){
-      tft.setCursor(0, 120);
+      tft.setCursor(160-6*3, 120);
       if(bzzTime == 0){
         bzzTime = time + bzzInterbal;
         tft.setTextSize(1);
@@ -148,7 +193,7 @@ void loop() {
   }
   else{
     if(wnkRightStatus == ON){
-      tft.setCursor(0, 120);
+      tft.setCursor(160-6*3, 128-8);
       if(bzzTime == 0){
         bzzTime = time + bzzInterbal;
         tft.setTextSize(1);
@@ -159,7 +204,7 @@ void loop() {
   }
 
   if(bzzTime > 0 && bzzTime <= time ){
-    tft.fillRect(0, 120, 6*3, 8, ST77XX_BLACK);
+    tft.fillRect(160-6*3, 120, 6*3, 8, ST77XX_BLACK);
     bzzTime = 0;
   }
    
@@ -178,30 +223,29 @@ void loop() {
     }
     posTime += posInterval;  
   //}
-
-  // ウインカー表示処理
-  //if(wnkTime <= time){
-    
-      if(wnkRightStatus == ON){
-        tft.setCursor(0, 50);
-        tft.print('<');
+    if(wnkRightStatus == ON){
+      if(isWnkRight == false){
+        tft.fillTriangle(160-32, 0, 160-32, 62, 160-0, 31, ST7735_YELLOW);
+        isWnkRight = true;
       }
-      else{
-        tft.fillRect(0, 50, 6*3, 8*3, ST77XX_BLACK);
-      }
+    }
+    else if(isWnkRight == true){
+      tft.fillRect(160-32, 0, 32, 63, ST77XX_BLACK);
+      isWnkRight = false;
+    }
 
-      if(wnkLeftStatus == ON){
-        tft.setCursor(6*3, 50);
-        tft.print('>');
+    if(wnkLeftStatus == ON){
+      if(isWnkLeft == false){
+        tft.fillTriangle(31, 0, 31, 62, 0, 31, ST7735_YELLOW);
+        isWnkLeft = true;
       }
-      else{
-        tft.fillRect(6*3, 50, 6*3, 8*3, ST77XX_BLACK);
-      }
+    }
+    else if(isWnkLeft == true){
+      tft.fillRect(0, 0, 32, 63, ST77XX_BLACK);
+      isWnkLeft = false;
+    }
 
-    wnkTime += wnkInterval;
-  //}
-
-  //delay(200);
+  wnkTime += wnkInterval;
 }
 
 //ギアポジション取得
