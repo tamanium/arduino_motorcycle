943487c8d7555eaec5ec51f6573de03a927a7049
tamanium <111425337+tamanium@users.noreply.github.com>
2024-10-09 18:01:12 +0900

    Update arduinoSource.ino

--- arduinoSource.ino
+++ arduinoSource.ino
@@ -1,16 +1,14 @@
+// --------------------ライブラリ--------------------
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include <SPI.h>
+
 // --------------------自作クラス・ピン定義--------------------
 //#include "Winker.h"
 #include "GearPosition.h"  //ギアポジションクラス
 #include "Pins.h"          //ピン設定
 #include "Define.h"        //値定義
 
-// --------------------ライブラリ--------------------
-#include <Adafruit_GFX.h>    // Core graphics library
-#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
-#include <SPI.h>
-
-// ビープ音を発生させるピン
-#define bzzPin 29
 
 // --------------------変数--------------------
 char exPos = '-';
