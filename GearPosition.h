550c8ced3c1c3305c4aadea76b5c9fdcd15fd90c
tamanium <111425337+tamanium@users.noreply.github.com>
2024-10-09 17:26:56 +0900

    Update GearPosition.h

--- GearPosition.h
+++ GearPosition.h
@@ -1,42 +1,15 @@
 #ifndef GEAR_POSITION_H_INCLUDE
 #define GEAR_POSITION_H_INCLUDE
 
-
-class GearPosition{
-  private:
-    // 読み取りピン
-    int pin;
-    // 表示文字
-    char word;
-
-  public:
-    GearPosition(int pin, char word);
-};
-/**
- *コンストラクタ
- */
-GearPosition::GearPosition(int p, char c){
-  // 読み取りピンを定義
-  pin = p;
-  // 表示文字定義
-  word = c;
-  // ピンモード定義
-  pinMode(pin, INPUT_PULLUP);
-}
-/**
- * 状態読み取り
- * LOWの場合true
- */
-bool GearPosition::getStatus(){
-  if(digitalRead(pin) == LOW){
-    return true;
+struct GearPosition{
+  // 読み取りピン番号  
+  int pin;
+  // 表示文字
+  char dispChar;
+  GearPosition(int pin, char dispChar){
+    this->pin = pin;
+    this->dispChar = dispChar;
   }
-  return false;
 }
-/**
- * 対応char取得
- */
-char GearPosition::getWord(){
-  return word;
-}
+
 #endif
