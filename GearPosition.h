fd39493ec1ec4dc523e86f8de50aab8d9560f1e7
tamanium <111425337+tamanium@users.noreply.github.com>
2024-10-09 17:27:48 +0900

    Update GearPosition.h

--- GearPosition.h
+++ GearPosition.h
@@ -2,10 +2,11 @@
 #define GEAR_POSITION_H_INCLUDE
 
 struct GearPosition{
-  // 読み取りピン番号  
-  int pin;
-  // 表示文字
-  char dispChar;
+  int pin;        // 読み取りピン番号
+  char dispChar;  // 表示文字
+  /**
+   * コンストラクタ
+  */
   GearPosition(int pin, char dispChar){
     this->pin = pin;
     this->dispChar = dispChar;
