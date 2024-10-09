e0d15a436e3ae80d68dd65a337b87285abc80e8c
tamanium <111425337+tamanium@users.noreply.github.com>
2024-10-09 13:41:29 +0900

    Update and rename AnyClass.h to Winker.h

--- Winker.h
+++ Winker.h
@@ -1,13 +1,12 @@
-#ifndef ANYCLASS_H_INCLUDE
-#define ANYCLASS_H_INCLUDE
+#ifndef WINKER_H_INCLUDE
+#define WINKER_H_INCLUDE
 
 class Winker{
-
   private:
     // ウインカー状態読み取りピン
-    int wnkPin;
+    int pin;
     // ウインカー点灯状態
-    bool exStatus;
+    bool status;
 
   public:
     Winker(int pin);
@@ -17,9 +16,9 @@ class Winker{
  */
 Winker::Winker(int pin){
   // ウインカー状態読み取りピンを定義
-  wnkPin = pin;
+  this->pin = pin;
   // ウインカー点灯状態を定義（デフォルト値：false）
-  exStatus = false;
+  this->Status = false;
 }
 
 #endif
