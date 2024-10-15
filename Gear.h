005648ff3694d983de1f0f04dc3e4ff2eff11492
tamanium <111425337+tamanium@users.noreply.github.com>
2024-10-15 16:43:24 +0900

    Update Gear.h

--- Gear.h
+++ Gear.h
@@ -12,42 +12,4 @@ class Gear{
 		char getChar();// 【Getter】表示値
 		bool isActive();// ポジションが自身か判定（表示値を参照渡し）
 };
-
-/**
- * デフォルトコンストラクタ
- */
-Gear::Gear(){
-  
-}
-
-/**
- * コンストラクタ
- * @param pin int型 表示値
- * @param dispChar char型 表示値
- */
-Gear::Gear(int pin, char dispChar){
-  this->pin = pin;
-  this->dispChar = dispChar;
-  pinMode(this->pin, INPUT_PULLUP);
-}
-
-/**
- * 【Getter】表示値
- * @return dispChar char型 表示値
- */
-char Gear::getChar(){
-	return dispChar;
-}
-
-/**
- * ポジションが自分自身か判定
- * @return 自分自身がポジションとなっている場合true
- */
-bool Gear::isActive(){
-	if(digitalRead(pin) == LOW){
-		return true;
-	}
-	return false;
-}
-
 #endif
