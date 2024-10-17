8c5dd12f09c614d54a51734626c350f855c8485d
tamanium <111425337+tamanium@users.noreply.github.com>
2024-10-17 14:04:24 +0900

    Update arduino_motorcycle.ino

--- arduino_motorcycle.ino
+++ arduino_motorcycle.ino
@@ -1,3 +1,9 @@
+// 課題解決1. TFTへデータ送信前後でcsピンのHIGH-LOWを変更
+// 課題解決2. SPI1を利用して接続
+
+
+
+
 // --------------------ライブラリ--------------------
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7735.h>
@@ -13,7 +19,9 @@
 // ディスプレイ
 #define TFT_MOSI  3
 #define TFT_SCLK  2
-#define TFT_CS    6 // ←変更候補 SPI0 CSnのピンに割り当ててみよう
+//#define TFT_MOSI  n // 解決2.にて使用 SPI1のやつを使用
+//#define TFT_SCLK  m // 解決2.にて使用 SPI1のやつを使用
+#define TFT_CS    6
 #define TFT_RST   8
 #define TFT_DC    7
 // 温度計
@@ -48,6 +56,7 @@ int timeFontSize = 2;
 
 // --------------------インスタンス--------------------
 Adafruit_ST7735 tft(&SPI, TFT_CS, TFT_DC, TFT_RST);// ディスプレイ設定
+//Adafruit_ST7735 tft(&SPI1, TFT_CS, TFT_DC, TFT_RST);// ディスプレイ設定 解決2.にて使用
 int gears[] = {POSN, POS1, POS2, POS3, POS4};
 GearPositions gearPositions(gears, sizeof(gears)/sizeof(int));// ギアポジション設定
 Winkers winkers(WNK_LEFT, WNK_RIGHT);// ウインカー設定
@@ -64,7 +73,6 @@ void setup(void) {
     //SPI.begin(TFT_CS);
 	// 温度計設定
 	thermoCouple.begin();
-	//thermoCouple.setSPIspeed(4000000); //4MHz
     
 	// ディスプレイ初期化・画面向き・画面リセット
     selectTFT();
@@ -122,11 +130,11 @@ void loop() {
 	}
 	// 各種表示処理
 	if(displayTime <= time){
-        selectTFT();
+        selectTFT();// 解決1.にて使用 SPI1のやつを使用
 		timeDisplay(time/1000, tft);
 		gearDisplay(gearPositions.getGear(), tft);
 		winkersDisplay(winkers, tft);
-        unselectTFT();
+        unselectTFT();// 解決1.にて使用 SPI1のやつを使用
 		displayTime += displayInterval;
 	}
 }
