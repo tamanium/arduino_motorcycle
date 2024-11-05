83f78f86d05b9336a64bf7edcf0a23535f3f8962
tamanium <111425337+tamanium@users.noreply.github.com>
2024-11-05 14:35:30 +0900

    Update GearPositions.h

--- GearPositions.h
+++ GearPositions.h
@@ -9,10 +9,10 @@ class GearPositions{
 		Gear gears[5];	// ギアクラス配列
 		char nowGear;	// 現在のギア表示値
 		int numOfGear;	// ギア個数
-        Adafruit_PCF8574 pcf;// IOエキスパンダ
+        Adafruit_PCF8574 *pcf;// IOエキスパンダポインタ
 	public:
-		GearPositions(int *pins, int len);  // コンストラクタ
-        void begin(Adafruit_PCF8574 *pcf);//ピン読み取り設定
+		GearPositions(int *pins, int len, Adafruit_PCF8574 *pcf);  // コンストラクタ
+        void setPCF(Adafruit_PCF8574 *pcf);//ピン読み取り設定
         void begin(uint8_t i2c_addr = PCF8574_I2CADDR_DEFAULT, TwoWire *wire = &Wire);
 		char getGear();  // 【Getter】表示値
 		void monitor(Adafruit_PCF8574 *pcf);  // 現在のギア表示値を更新
