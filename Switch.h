7898b6f4887a3973f4238a0898e9c4587254efe1
tamanium <111425337+tamanium@users.noreply.github.com>
2025-03-21 22:19:58 +0900

    Switch.h を更新

--- Switch.h
+++ Switch.h
@@ -1,1 +1,18 @@
-a
+#ifndef SWITCH_H_INCLUDE
+#define SWITCH_H_INCLUDE
+#include <Arduino.h>
+#include <Adafruit_PCF8574.h>
+
+// スイッチクラス
+class Switch{
+	private:
+		int pinSwitch;		// スイッチ読み取りピン
+		bool status;	// スイッチ状態
+        Adafruit_PCF8574 *pcf;// IOエキスパンダ
+	public:
+        Switch(int pinSwitch, Adafruit_PCF8574 *pcf);// コンストラクタ
+        bool getStatus();  //【Getter】ウインカー状態
+		void monitor();// ウインカー状態更新
+};
+
+#endif
