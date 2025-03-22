2bcdd5eac294513ef09dc5d30f184b6a04576968
tamanium <111425337+tamanium@users.noreply.github.com>
2025-03-22 23:58:05 +0900

    GearPositions.cpp を更新

--- GearPositions.cpp
+++ GearPositions.cpp
@@ -18,29 +18,7 @@ GearPositions::GearPositions(int *pins, int len, Adafruit_PCF8574 *pcf){
   this->numOfGear = len;
   this->nowGear = '-';
   this->pcf = pcf;
-	
 }
-/**
- * 通信開始
- * @param *pcf Adafruit_PCF8574クラス IOエキスパンダ
-void GearPositions::begin(Adafruit_PCF8574 *pcf){
-    for(int i=0;i<5;i++){
-        this->gears[i].begin(pcf);
-    }
-}
- */
-
-/**
- * 通信開始
- * @param i2c_addr uint8_t型  IOエキスパンダ
- * @param *wire TwoWireクラス IOエキスパンダ
-void GearPositions::begin(uint8_t i2c_addr, TwoWire *wire){
-    this->pcf.begin(i2c_addr, wire);
-    for(int i=0;i<5;i++){
-        this->gears[i].begin(&(this->pcf));
-    }
-}
- */
 
 /**
  * 【Getter】表示値
@@ -50,42 +28,6 @@ char GearPositions::getGear(){
 	return this->nowGear;
 }
 
-/**
- * 現在のギア表示値を更新
- * @param *pcf Adafruit_PCF8574型  IOエキスパンダ
-void GearPositions::monitor(Adafruit_PCF8574 *pcf){
-	// カウンタ・直前ギア
-	static int counter = 0;
-	static char bufferGear = '-';
-	// 現在のギア表示値を宣言
-	char newGear = '-';
-	// ギア配列でループし、現在のギア表示値を取得
-	for(int i=0; i<this->numOfGear; i++){
-		if(this->gears[i].isActive(pcf) == true){
-			newGear = this->gears[i].getChar();
-			break;
-		}
-	}
-	
-	// 直前ギアと取得ギアが異なる場合
-	if(bufferGear != newGear){
-		// 直前ギアを上書き、カウンタリセット
-		bufferGear = newGear;
-		counter = 0;
-	}
-	// 現在ギアと直前ギアが異なる場合
-	else if(this->nowGear != bufferGear){
-		counter++;
-	}
-	
-	// 5カウント以上の場合
-	if(5 <= counter){
-		// 現在ギアに直前ギアを代入し、カウンタをリセット
-		this->nowGear = bufferGear;
-		counter = 0;
-	}
-}
- */
 /**
  * 現在のギア表示値を更新
  */
