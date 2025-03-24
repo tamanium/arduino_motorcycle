6eb277e91d28286709e461e61459d21045485f94
tamanium <111425337+tamanium@users.noreply.github.com>
2025-03-25 08:14:18 +0900

    Winker.h を更新

--- Winker.h
+++ Winker.h
@@ -9,13 +9,13 @@ class Winkers{
 		int pinLeft;		// 左ウインカー読み取りピン
 		int pinRight;		// 右ウインカー読み取りピン
 		bool statusLR[2];	// ウインカー状態
-        Adafruit_PCF8574 *pcf;// IOエキスパンダ
+		Adafruit_PCF8574 *pcf;// IOエキスパンダ
 	public:
-        // コンストラクタ
+		// コンストラクタ
 		Winkers(int pinLeft, int pinRight, Adafruit_PCF8574 *pcf);
-        // ウインカー状態取得
-        bool getStatus(int i);  
-        // ウインカー状態更新
+		// ウインカー状態取得
+		bool getStatus(int i);  
+		// ウインカー状態更新
 		void monitor();
 };
 
