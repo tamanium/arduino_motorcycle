0a8c20744a6a0d5a9f3aa6111ba855b37273eaad
kazuma.tamahashi <kazuma.tamahashi@sys21.co.jp>
2025-04-01 13:03:18 +0900

    edit w

--- CommonDefine.h
+++ CommonDefine.h
@@ -1,14 +1,14 @@
 #ifndef COMMON_DEFINE_H_INCLUDE
 	#define COMMON_DEFINE_H_INCLUDE
 
-	#define ON true
-	#define OFF false
+	#define ON			true
+	#define OFF			false
 
-	#define KEY_DOWN true
-	#define KEY_UP false
+	#define KEY_DOWN	true
+	#define KEY_UP	false
 
-	#define LEFT 0
-	#define RIGHT 1
+	#define LEFT	0
+	#define RIGHT	1
 
 	#define MONTH	0
 	#define DAY		1
