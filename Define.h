0362b326a19cbb00102f35a549ffc93766df366c
tamanium <111425337+tamanium@users.noreply.github.com>
2025-04-19 22:34:24 +0900

    Define.h を更新

--- Define.h
+++ Define.h
@@ -63,6 +63,13 @@
 			} WNK;
 		} IOEXP;
 	} PIN;
+	
+	// 表示設定
+struct PrintProperty {
+	int x = 0;						// x座標
+	int y = 0;						// y座標
+	int size = 1;					// フォント倍率	（デフォルト：1）
+};
 
 	/**
 	 * x座標出力（画面右端原点、左向き）
