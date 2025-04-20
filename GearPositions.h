d1aacfba310797ac46aa6f3184f35d7857ad0222
tamanium <111425337+tamanium@users.noreply.github.com>
2025-04-20 23:45:34 +0900

    GearPositions.h を更新

--- GearPositions.h
+++ GearPositions.h
@@ -6,12 +6,13 @@
 	// ギアポジションクラス
 	class GearPositions{
 		private:
-			IOPin gears[5];			// ギアクラス配列
-			char nowGear;			// 現在のギア表示値
-			int GearNum;			// ギア個数
+			IOPin gears[5]; // ギアクラス配列
+			char nowGear;   // 現在のギア表示値
+			int GearNum;    // ギア個数
 		public:
 			// コンストラクタ
 			GearPositions(int *pins, int len, Adafruit_PCF8574 *pcf);  
+			begin();
 			// 表示値取得
 			char getGear();
 			// ギア状態更新
