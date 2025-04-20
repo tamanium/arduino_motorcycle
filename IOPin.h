831284cd62139a6968cbc77b725d50de0adfeed3
tamanium <111425337+tamanium@users.noreply.github.com>
2025-04-20 23:34:34 +0900

    IOPin.h を更新

--- IOPin.h
+++ IOPin.h
@@ -7,13 +7,15 @@
 	// ギアポジションクラス
 	class IOPin{
 		private:
-			int pin;		// 読み取りピン番号
-			char dispChar;	// 表示値
-			Adafruit_PCF8574 *pcf;
+			int pin;               // 読み取りピン番号
+			char dispChar;         // 表示値
+			Adafruit_PCF8574 *pcf; // IOエキスパンダ
 		public:
 			// コンストラクタ
 			IOPin();
 			IOPin(int p, char c, Adafruit_PCF8574 *pcf);
+			// 動作開始
+			begin(int mode);
 			// 表示値取得
 			char getChar();
 			// 読み取り
